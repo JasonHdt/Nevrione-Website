ca8b8a4eeb4f340e9aa295d397e248162f24a3a1
JasonHdt <jasonhaudot@gmail.com>
2026-05-29 17:50:24 +0200

    Map functionnal

--- js/leaflet.js
+++ js/leaflet.js
@@ -14,14 +14,95 @@ const map = L.map('frostlands', {
     maxBoundsViscosity: 1.0
 });
 
-var c = new L.Control.Coordinates();
-
-c.addTo(map);
-
-map.on('click', function(e) {
-	c.setCoordinates(e);
-});
-
-L.imageOverlay('../assets/26-05-29.svg', bounds).addTo(map);
 
 map.fitBounds(bounds);
+
+// ======================
+// Coordonnées au clic
+// ======================
+/*
+map.on('click', function(e) {
+
+    const x = Math.round(e.latlng.lng);
+    const y = Math.round(e.latlng.lat);
+
+    console.log(`x: ${x}, y: ${y}`);
+
+});
+*/
+
+// ======================
+// Marqueurs
+// ======================
+
+const size = 100
+
+// ICON TYPE
+const NORNFANG = L.icon({
+    iconUrl: '../assets/STAMP_nornfang.webp',
+    iconSize: [size, size],
+    iconAnchor: [size/2, size/2],
+    popupAnchor: [-3, -76],
+});
+
+const CAVE = L.icon({
+    iconUrl: '../assets/STAMP_cave.webp',
+    iconSize: [size, size],
+    iconAnchor: [size/2, size/2],
+    popupAnchor: [-3, -76]
+});
+
+const RUIN = L.icon({
+    iconUrl: '../assets/STAMP_ruin.webp',
+    iconSize: [size, size],
+    iconAnchor: [size/2, size/2],
+    popupAnchor: [-3, -76]
+});
+// .................
+
+map.createPane('roads');
+map.getPane('roads').style.zIndex = 200;
+
+map.createPane('nodes');
+map.getPane('nodes').style.zIndex = 400;
+
+
+
+// 29-05-26
+
+// NODES
+
+const node0 = L.marker([300, 1000], {icon: NORNFANG}).addTo(map);
+const node1 = L.marker([600, 1000], {icon: CAVE}).addTo(map);
+
+// Lignes
+
+L.polyline([
+    [300, 1000],
+    [600, 1000]
+], {
+    road: '0 -> 1',
+    pane: 'roads',
+    color: 'yellow',
+    weight: 10
+}).addTo(map);
+
+// PopUP
+node0.bindPopup(`Ambrore`);
+node1.bindPopup("Grotte des contrebandiers");
+
+/*
+// Next Session
+const node2 = L.marker([900, 700], {icon: RUIN}).addTo(map);
+L.polyline([
+    [600, 1000],
+    [900, 700]
+], {
+    road: '0 -> 1',
+    pane: 'roads',
+    color: 'yellow',
+    weight: 10
+}).addTo(map);
+*/
+
+
