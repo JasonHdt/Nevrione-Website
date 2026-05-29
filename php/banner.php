10b98a9d818bfe95e437e217d2d3ed4eec725b4b
JasonHdt <jasonhaudot@gmail.com>
2026-05-29 16:27:13 +0200

    Disabling PHP to keep HTML

--- php/banner.php
+++
@@ -1,8 +0,0 @@
-
-</head>
-<body>
-<div class="banner_container">
-            <div class="banner_logo">
-                <a href="../index.html"><img src="../assets/logo.svg" alt=""></a>
-            </div>
-    </div>
