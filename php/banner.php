873ef84dd84019f6a29e71b54b9326f0969e2579
JasonHdt <jasonhaudot@gmail.com>
2026-05-29 16:24:41 +0200

    fix Index.html

--- php/banner.php
+++ php/banner.php
@@ -3,6 +3,6 @@
 <body>
 <div class="banner_container">
             <div class="banner_logo">
-                <a href="../index.php"><img src="../assets/logo.svg" alt=""></a>
+                <a href="../index.html"><img src="../assets/logo.svg" alt=""></a>
             </div>
     </div>
