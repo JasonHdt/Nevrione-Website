5fed2f84a5bf938c17933a789f9cbfa13b7ee2ab
JasonHdt <jasonhaudot@gmail.com>
2026-05-29 07:26:35 +0200

    260529 - Accueil

---
+++ index.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+include 'php/head.php'
+?>
+<title>Page d'accueil</title>
+</head>
+<body>
+    <div class="banner_container">
+            <div class="banner_logo">
+                <a href="index.html"><img src="assets/logo.svg" alt=""></a>
+            </div>
+    </div>
+    <div class="hero">
+        <div class="composants_container">
+            <div class="composant_button_container">
+                <div class="composant_button_image">
+                    <img src="assets/treasure-map.svg" alt="treasure_map.svg">
+                </div>
+                <div class="composant_button_title">
+                    Carte des Frostlands
+                </div>
+            </div>
+                <div class="composant_button_container">
+                    <div class="composant_button_image">
+                        <img src="assets/castle.svg" alt="treasure_map.svg">
+                    </div>
+                    <div class="composant_button_title">
+                        Royaume
+                    </div>
+                </div>
+            <div class="composant_button_container">
+                <div class="composant_button_image">
+                    <img src="assets/cultist.svg" alt="treasure_map.svg">
+                </div>
+                <div class="composant_button_title">
+                    Dramatis Personae
+                </div>
+            </div>
+            <div class="composant_button_container">
+                <div class="composant_button_image">
+                    <img src="assets/potion-ball.svg" alt="treasure_map.svg">
+                </div>
+                <div class="composant_button_title">
+                    Objets
+                </div>
+            </div>
+            <div class="composant_button_container">
+                <div class="composant_button_image">
+                    <img src="assets/secret-book.svg" alt="treasure_map.svg">
+                </div>
+                <div class="composant_button_title">
+                    Règles
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="legal.html">Légalité</a>
+</body>
+</html>
