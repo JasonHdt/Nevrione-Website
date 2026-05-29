9a37628541804bf89ef483eb491d0d8084c7c2d8
JasonHdt <jasonhaudot@gmail.com>
2026-05-29 07:27:28 +0200

    Folder changed to MAMP

--- php/head.php
+++ php/head.php
@@ -1,13 +1,8 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page d'accueil</title>
     <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    Hello World
-    <a href="legal.html">Légalité</a>
-</body>
-</html>
+
